b6e56c72939321149eab13be146f55650fec2036
divetri <54519415+divetri@users.noreply.github.com>
2019-12-12 23:25:45 +0700

    Update paint_slave.ino
    Make every coordinate pressed to be saved in SD Card from NodeMCU to SD Card Module

--- paint_slave.ino
+++ paint_slave.ino
@@ -1,60 +1,58 @@
+#include <SD.h>
+#define CS_PIN D8
 String dataIn;
-String dt[10];
-int i;
-boolean parsing=false;
+String dt[2];
+int i, px, py;
+boolean parsing = false;
+
 void setup() {
-   Serial.begin(9600);
-   dataIn="";
+  Serial.begin(9600);
+  if (!SD.begin(CS_PIN)) {
+    Serial.println("Falha, verifique se o cartão está presente.");
+    //programa encerrrado
+    return;
+  }
+  dataIn = "";
 }
 void loop() {
-  if(Serial.available()>0) {
+  if (Serial.available() > 0) {
     char inChar = (char)Serial.read();
     dataIn += inChar;
     if (inChar == '\n') {
-    parsing = true;
+      parsing = true;
+    }
   }
-}
- 
-if(parsing){
+
+  if (parsing) {
     parsingData();
-    parsing=false;
-    dataIn="";
+    parsing = false;
+    dataIn = "";
   }
 }
-void parsingData(){
-int j=0;
- 
-//kirim data yang telah diterima sebelumnya
-Serial.print("data masuk : ");
-Serial.print(dataIn);
-Serial.print("\n");
- 
-//inisialisasi variabel, (reset isi variabel)
-dt[j]="";
-//proses parsing data
-for(i=1;i<dataIn.length();i++){
-//pengecekan tiap karakter dengan karakter (#) dan (,)
-if ((dataIn[i] == '#') || (dataIn[i] == ','))
-{
-//increment variabel j, digunakan untuk merubah index array penampung
-j++;
-dt[j]="";       //inisialisasi variabel array dt[j]
-}
-else
-{
-//proses tampung data saat pengecekan karakter selesai.
-dt[j] = dt[j] + dataIn[i];
-}
-}
- 
-//kirim data hasil parsing
-Serial.print("data 1 : ");
-Serial.print(dt[0].toInt());
-Serial.print("\n");
-Serial.print("data 2 : ");
-Serial.print(dt[1].toInt());
-Serial.print("\n");
-Serial.print("data 3 : ");
-Serial.print(dt[2].toInt());
-Serial.print("\n\n");
+void parsingData() {
+  int j = 0;
+  dt[j] = "";
+  for (i = 0; i < dataIn.length(); i++) {
+    if (dataIn[i] == ',')
+    {
+      j++;
+      dt[j] = "";
+    }
+    else
+    {
+      dt[j] = dt[j] + dataIn[i];
+    }
+  }
+  px = dt[0].toInt();
+  py = dt[1].toInt();
+  File testfile = SD.open("testdata.txt", FILE_WRITE);
+  if (testfile) {
+    testfile.print("x : ");
+    testfile.print(px);
+    testfile.print("\n");
+    testfile.print("y : ");
+    testfile.print(py);
+    testfile.print("\n");
+
+  }
 }
